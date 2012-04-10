416d28a2f5599e1224831b5f40ef35aaabccc37c
Steve Tipton <steve.tipton@employmentguide.com>
2012-04-10 11:36:07 -0400

    add accessibility specs

---
+++ spec/factories/teams.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+
+  factory :team do
+    name          "Carolina Panthers"
+    nickname      "Panthers"
+    abbreviation  "CAR"
+    city          "Charlotte"
+    conference    "NFC"
+    division      "South"
+  end
+end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -6,10 +6,60 @@ describe Team do
 
     it { should respond_to(:name) }
     it { should respond_to(:nickname) }
-    it { should respond_to(:city) }
     it { should respond_to(:abbreviation) }
+    it { should respond_to(:city) }
     it { should respond_to(:conference) }
     it { should respond_to(:division) }
+
+    describe "accessibility" do
+
+      before(:each) do
+        @attrs = FactoryGirl.attributes_for(:team)
+        @team = Team.create(@attrs)
+      end
+
+      describe "name" do
+
+        it "should be accessible" do
+          @team.name.should eq @attrs[:name]
+        end
+      end
+
+      describe "nickname" do
+
+        it "should be accessible" do
+          @team.nickname.should eq @attrs[:nickname]
+        end
+      end
+
+      describe "abbreviation" do
+
+        it "should be accessible" do
+          @team.abbreviation.should eq @attrs[:abbreviation]
+        end
+      end
+
+      describe "city" do
+
+        it "should be accessible" do
+          @team.city.should eq @attrs[:city]
+        end
+      end
+
+      describe "conference" do
+
+        it "should be accessible" do
+          @team.conference.should eq @attrs[:conference]
+        end
+      end
+
+      describe "division" do
+
+        it "should be accessible" do
+          @team.division.should eq @attrs[:division]
+        end
+      end
+    end
   end
 
   describe "validations" do
@@ -18,8 +68,8 @@ describe Team do
 
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:nickname) }
-    it { should validate_presence_of(:city) }
     it { should validate_presence_of(:abbreviation) }
+    it { should validate_presence_of(:city) }
     it { should validate_presence_of(:conference) }
     it { should validate_presence_of(:division) }
   end
