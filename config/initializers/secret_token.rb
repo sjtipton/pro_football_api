93e8792ecb02f2c57a0e5d4ea6fc581eca57d537
Steve Tipton <steve.tipton@employmentguide.com>
2012-04-10 09:29:30 -0400

    initial commit for new rails app: pro_football_api

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProFootballApi::Application.config.secret_token = 'bc06d25ed723a10a8a0ec954fac9eba14ff809291fd82739cdcf4b81306699213b1a5793387519effd30ebed8951a91e9b511ed44fc78a6170b731e57251cb1b'
