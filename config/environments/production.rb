a1b1bcd9f743253402bd6062fb988ff87ce9babf
Steve Tipton <steve.tipton@employmentguide.com>
2012-04-10 22:27:52 -0400

    sendgrid configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,15 @@ ProFootballApi::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: "25",
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: ENV['SENDGRID_DOMAIN']
+  }
 
   # Devise mailer
   config.action_mailer.default_url_options = { :host => 'pro-football-api.herokuapp.com' }
